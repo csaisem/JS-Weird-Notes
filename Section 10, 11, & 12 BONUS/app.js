53154ee5f78d55fddb60bd28b96503c349945e34
Charles-Desktop <csaisem@yahoo.com>
2020-09-25 21:31:13 -0700

    Finished the rest of JS Weird Parts

---
+++ Section 10, 11, & 12 BONUS/app.js
@@ -0,0 +1,4 @@
+// Transpile: Convert the syntax of one programming langauge to another
+// TypeScript & Traceur is one of these 
+// The rest of the short minute videos introduce the next verson of JS and
+// where to find its features. The end.s
